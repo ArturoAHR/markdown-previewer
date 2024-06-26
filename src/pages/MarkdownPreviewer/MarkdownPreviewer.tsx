55e2acb0f139dc0399fb9dd55965b95fd15325be
Arturo Hernández <arturoaureliohr@gmail.com>
2024-06-26 01:13:23 -0400

    Add customization option to select editor theme

--- src/pages/MarkdownPreviewer/MarkdownPreviewer.tsx
+++ src/pages/MarkdownPreviewer/MarkdownPreviewer.tsx
@@ -1,14 +1,16 @@
 import { highlight, languages } from "prismjs";
 import "prismjs/components/prism-markdown";
-import "prismjs/themes/prism.css"; // Import a PrismJS theme
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Editor from "react-simple-code-editor";
 
 import { useDebounce } from "../../hooks/useDebounce";
 import { useMarked } from "../../hooks/useMarked";
 
 import { Select } from "../../components/Select/Select";
-import { MarkdownEditorSettings } from "../../types/markdown-editor-settings";
+import {
+  MarkdownEditorSettings,
+  MarkdownEditorTheme,
+} from "../../types/markdown-editor-settings";
 import "./MarkdownPreviewer.css";
 
 export const MarkdownPreviewer = () => {
@@ -18,9 +20,32 @@ export const MarkdownPreviewer = () => {
 
   const [markdownEditorSettings, setMarkdownEditorSettings] =
     useState<MarkdownEditorSettings>({
-      fontSize: 14,
-      theme: "prism",
+      fontSize: 16,
+      theme: MarkdownEditorTheme.Prism,
     });
+
+  /**
+   * Swaps the current link used to load the theme for another one with the theme selected.
+   */
+  const loadTheme = async (theme: string) => {
+    const themeId = "prism-theme-stylesheet";
+    const existingLink = document.getElementById(themeId);
+
+    if (existingLink) {
+      existingLink.remove();
+    }
+
+    const link = document.createElement("link");
+    link.id = themeId;
+    link.rel = "stylesheet";
+    link.href = `themes/${theme}.min.css`;
+
+    document.head.appendChild(link);
+  };
+
+  useEffect(() => {
+    loadTheme(markdownEditorSettings.theme);
+  }, [markdownEditorSettings]);
 
   const downloadMarkdownHtml = () => {
     const element = document.createElement("a");
@@ -38,20 +63,30 @@ export const MarkdownPreviewer = () => {
   };
 
   const fontSizeOptions = useMemo(() => {
-    const baseFontSize = 8;
-    const options = [];
+    const options = [8, 10, 12, 14, 16, 18, 20, 22, 24, 32, 40, 48, 72];
 
-    for (let i = 0; i <= 10; i++) {
-      const fontSize = baseFontSize + i * 2;
-
-      options.push({
-        label: `${fontSize}px`,
-        value: fontSize,
-      });
-    }
-
-    return options;
+    return options.map((option) => ({
+      value: option,
+      label: `${option}px`,
+    }));
   }, []);
+
+  const themeOptions = useMemo(() => {
+    return Object.entries(MarkdownEditorTheme).map(([key, value]) => ({
+      value,
+      label: key,
+    }));
+  }, []);
+
+  // Background colors for each theme as the Prism themes don't have a background color.
+  const themeBackgroundColors = {
+    [MarkdownEditorTheme.Prism]: "#f8f8f8",
+    [MarkdownEditorTheme.Dark]: "#000",
+    [MarkdownEditorTheme.Okaidia]: "#272822",
+    [MarkdownEditorTheme.SolarizedLight]: "#fdf6e3",
+    [MarkdownEditorTheme.Tomorrow]: "#f8f8f8",
+    [MarkdownEditorTheme.Coy]: "#fbf7f3",
+  };
 
   const changeEditorSettings = (settings: Partial<MarkdownEditorSettings>) => {
     setMarkdownEditorSettings({
@@ -64,17 +99,25 @@ export const MarkdownPreviewer = () => {
     <div className="markdown-previewer">
       <div className="markdown-previewer-editor">
         <div className="markdown-previewer-editor-header">
-          <div className="markdown-previewer-editor-header-label">
-            Introduce your text:
+          <div className="markdown-previewer-editor-header-label">Editor</div>
+          <div className="markdown-previewer-editor-header-settings">
+            <Select
+              className="markdown-previewer-editor-header-select font-size"
+              options={fontSizeOptions}
+              value={markdownEditorSettings.fontSize}
+              onChange={(fontSize) =>
+                changeEditorSettings({ fontSize: +fontSize })
+              }
+            />
+            <Select
+              className="markdown-previewer-editor-header-select theme"
+              options={themeOptions}
+              value={markdownEditorSettings.theme}
+              onChange={(theme) =>
+                changeEditorSettings({ theme: theme as MarkdownEditorTheme })
+              }
+            />
           </div>
-          <Select
-            className="markdown-previewer-editor-header-select font-size"
-            options={fontSizeOptions}
-            value={markdownEditorSettings.fontSize}
-            onChange={(fontSize) =>
-              changeEditorSettings({ fontSize: +fontSize })
-            }
-          />
         </div>
         <Editor
           className="markdown-previewer-editor-textarea"
@@ -86,14 +129,14 @@ export const MarkdownPreviewer = () => {
             fontSize: markdownEditorSettings.fontSize,
             lineHeight: 1.5,
             overflow: "auto",
+            backgroundColor:
+              themeBackgroundColors[markdownEditorSettings.theme],
           }}
         />
       </div>
       <div className="markdown-previewer-preview">
         <div className="markdown-previewer-preview-header">
-          <div className="markdown-previewer-preview-header-label">
-            Preview:
-          </div>
+          <div className="markdown-previewer-preview-header-label">Preview</div>
           <button
             className="markdown-previewer-preview-header-button download"
             onClick={downloadMarkdownHtml}
--- src/types/markdown-editor-settings.ts
+++ src/types/markdown-editor-settings.ts
@@ -1,4 +1,13 @@
 export type MarkdownEditorSettings = {
   fontSize: number;
-  theme: string;
+  theme: MarkdownEditorTheme;
 };
+
+export enum MarkdownEditorTheme {
+  Prism = "prism",
+  Dark = "prism-dark",
+  Okaidia = "prism-okaidia",
+  SolarizedLight = "prism-solarizedlight",
+  Tomorrow = "prism-tomorrow",
+  Coy = "prism-coy",
+}
