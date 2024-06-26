1e3480d08cc7d51b462ad099875dd8a8aacee963
Arturo Hernández <arturoaureliohr@gmail.com>
2024-06-25 23:01:44 -0400

    Add export button to download the produced HTML from the preview

--- src/pages/MarkdownPreviewer/MarkdownPreviewer.tsx
+++ src/pages/MarkdownPreviewer/MarkdownPreviewer.tsx
@@ -9,6 +9,21 @@ export const MarkdownPreviewer = () => {
   const debouncedInputText = useDebounce(inputText, 500);
   const { htmlMarkdown } = useMarked({ inputText: debouncedInputText });
 
+  const downloadMarkdownHtml = () => {
+    const element = document.createElement("a");
+    const file = new Blob([htmlMarkdown.__html], { type: "text/html" });
+
+    element.href = URL.createObjectURL(file);
+    element.download = "markdown.html";
+
+    document.body.appendChild(element);
+
+    element.click();
+
+    URL.revokeObjectURL(element.href);
+    document.body.removeChild(element);
+  };
+
   return (
     <div className="markdown-previewer">
       <div className="markdown-previewer-editor">
@@ -22,7 +37,17 @@ export const MarkdownPreviewer = () => {
         />
       </div>
       <div className="markdown-previewer-preview">
-        <div className="markdown-previewer-preview-label">Preview:</div>
+        <div className="markdown-previewer-preview-header">
+          <div className="markdown-previewer-preview-header-label">
+            Preview:
+          </div>
+          <button
+            className="markdown-previewer-preview-header-button download"
+            onClick={downloadMarkdownHtml}
+          >
+            Export
+          </button>
+        </div>
         <div
           className="markdown-previewer-preview-html"
           dangerouslySetInnerHTML={htmlMarkdown}
