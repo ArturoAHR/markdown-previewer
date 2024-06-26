b8acf7c727037a937962b9f59fa9627a1d72e41b
Arturo Hernández <arturoaureliohr@gmail.com>
2024-06-25 21:17:46 -0400

    Add layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+import "./Layout.css";
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="layout">
+      <div className="layout-header">Markdown Previewer</div>
+      <div className="layout-content">{children}</div>
+    </div>
+  );
+};
